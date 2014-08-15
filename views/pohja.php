a0725e0e0c11a573fb20bb2433c3b87b9ed38d82
Anu Salminen <aesalmin@gmail.com>
2014-08-15 03:09:16 +0300

    aloitellaan mvc-mallin mukaisuutta

--- views/pohja.php
+++ views/pohja.php
@@ -49,7 +49,8 @@
          * joka haetaan sopivasta näkymätiedostosta.
          * Oikean näkymän tiedostonimi on tallennettu muuttujaan $sivu.
          */
-        require 'views/'.$sivu.'.php'; 
+        require $sivu.'.php'; 
+//         require 'views/'.$sivu.'.php';        
       ?>
 
         </div> 
